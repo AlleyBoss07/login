8f1125b41415deb9a76d828a6b8d18e42f0e4245
AlleyBoss07 <122385706+AlleyBoss07@users.noreply.github.com>
2023-06-07 12:22:53 +0530

    update

--- script.js
+++ script.js
@@ -1,44 +1,31 @@
 $(document).ready(function(){
- $("#logbtn").click(function() {
-    if($("#uname").val()=="" && $("#pwd").val()==""){
-        alert("Please enter uname and password")
+  const abc=()=>
+        {
+            if($("#uname").val()=="" && $("#pwd").val()==""){
+                alert("Please enter uname and password")
+            }
+            else if($("#pwd").val()=="" || $("#uname").val()==""){
+                if($("#uname").val()==""){
+                    alert("Please enter your username");
+                }
+                else{
+                    alert("Please enter your password ");
+                }     
+               
+            }
+            else{
+                alert("Welcome  "+$("#uname").val());
+                    $("#uname").val("");
+                 $("#pwd").val("");
+            }
+          
+         }
+    
+ $("#logbtn").click(abc);
+ $(document).keypress((e)=>{
+    console.log(e)
+    if(e.keyCode==13){
+        abc();
     }
-    else if($("#pwd").val()=="" || $("#uname").val()==""){
-        if($("#uname").val()==""){
-            alert("Please enter your username");
-        }
-        else{
-            alert("Please enter your password ");
-        }     
-       
-    }
-    else{
-        alert("Welcome  "+$("#uname").val());
-    }
-    $("#uname").val("");
-    $("#pwd").val("");
- })
-//  var text=document.querySelectorAll("input")
-//  text.addEventListener("keypress",(event)=>{
-//     if(event.key=="Enter"){
-//       event.preventDefault();
-//       if ($("#uname").val() == "" && $("#pwd").val() == "") {
-//         alert("Please enter uname and password")
-//     }
-//     else if ($("#pwd").val() == "" || $("#uname").val() == "") {
-//         if ($("#uname").val() == "") {
-//             alert("Please enter your username");
-//         }
-//         else {
-//             alert("Please enter your password ");
-//         }
-
-//     }
-//     else {
-//         alert("Welcome  " + $("#uname").val());
-//     }
-//     $("#uname").val("");
-//     $("#pwd").val("");
-//     }
-//  })
+ });
 })
